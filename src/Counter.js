b21ccfefada03fc0d74df2929a9b7c19a551d0f5
Dmitry G. Anderson <dandgerson@gmail.com>
2022-10-08 21:31:42 +0300

    setState object

---
+++ src/Counter.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {count: 0}
+
+  increment = () => {
+    this.setState({count: this.state.count + 1})
+    this.setState({count: this.state.count + 1})
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrement = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  reset = () => {
+    this.setState({count: 0})
+  }
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div className="counter">
+      <div className="count">{count}</div>
+      <div className="controls">
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
